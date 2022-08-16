2f4b1e8eb640239701bb5fd2a08958c5f9a0a7e3
James Owiti Maurice <james.owiti@student.moringaschool.com>
2022-08-16 17:50:29 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+// Write your code here !
+let num1=2;
+let num2=31;
 
-// Write your code here
+const multiply =num1 * num2;
+
+const random = 2;
+
+let num3=24;
+let num4=6;
+
+const mod=num3/num4;
+let max =20;
